eecbd505dd293d93f662126fe174520005927092
邓渊 <330715487@qq.com>
2016-09-13 10:12:05 +0800

    源码上传

---
+++ footer.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Leaf , Codilight_Lite
+ */
+?>
+		</div> <!--#content-inside-->
+	</div><!-- #content -->
+	<div class="footer-shadow container">
+		<div class="row">
+			<div class="col-md-12">
+				<img src="<?php echo get_template_directory_uri().'/assets/images/footer-shadow.png' ?>" alt="" />
+			</div>
+		</div>
+	</div>
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="container">
+
+			<?php if ( has_nav_menu( 'footer' ) ): ?>
+			<div class="footer-navigation">
+				<?php wp_nav_menu( array('theme_location' => 'footer', 'container' => 'footer-menu', 'fallback_cb' => false ) ); ?>
+			</div>
+			<?php endif; ?>
+
+			<div class="site-info">
+				Copyright &copy; 2015-2016
+		      	<strong><a rel="nofollow"  href="http://www.py40.com/" target="_blank" > python学习</a></strong>&nbsp;
+		    	<strong><a rel="nofollow"  href="http://www.py40.com/" target="_blank">py40.com</a></strong> All Rights Reserved. </div><!-- .site-info -->
+
+		</div>
+	</footer><!-- #colophon -->
+
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+<div class="fixed-btn">
+<div id="full" class = "fixed-btn"> 
+	<a><img src="<?php echo get_template_directory_uri().'/assets/images/top.png' ?>" border=0 alt="返回顶部"></a> 
+</div>
+<div>
+
+<script type = "text/javascript" >
+	var isie6 = window.XMLHttpRequest ? false: true;
+	function newtoponload() {
+	    var c = document.getElementById("full");
+	    function b() {
+	        var a = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+	        if (a > 0) {
+	            if (isie6) {
+	                c.style.display = "none";
+	                clearTimeout(window.show);
+	                window.show = setTimeout(function() {
+	                    var d = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop;
+	                    if (d > 0) {
+	                        c.style.display = "block";
+	                        c.style.top = (400 + d) + "px"
+	                    }
+	                },
+	                300)
+	            } else {
+	                c.style.display = "block"
+	            }
+	        } else {
+	            c.style.display = "none"
+	        }
+	    }
+	    if (isie6) {
+	        c.style.position = "absolute"
+	    }
+	    window.onscroll = b;
+	    b()
+	}
+	if (window.attachEvent) {
+	    window.attachEvent("onload", newtoponload)
+	} else {
+	    window.addEventListener("load", newtoponload, false)
+	}
+	document.getElementById("full").onclick = function() {
+	    window.scrollTo(0, 0)
+	}; 
+</script>
+
+</body>
+</html>
---
+++ template-parts/content-grid.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template part for displaying posts with grid style.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Codilight_Lite
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'col-md-6 col-sm-12' ); ?>>
+    <div class="entry-thumb">
+        <a href="<?php echo esc_url( get_permalink() ); ?>" title="<?php the_title(); ?>">
+		<img  style="width:325px;height:170px"  src="<?php echo post_thumbnail_src()?>">
+		</a>
+        <?php
+        $category = get_the_category();
+        if($category[0]){
+            echo '<a class="entry-category" href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>';
+        }
+        ?>
+    </div>
+    <div class="entry-detail">
+        <header class="entry-header">
+    		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+    		<?php if ( 'post' === get_post_type() ) codilight_lite_meta_1();?>
+    	</header><!-- .entry-header -->
+
+    	<div class="entry-excerpt">
+    		<?php echo codilight_lite_excerpt(70); ?>
+    	</div><!-- .entry-content -->
+    </div>
+</article><!-- #post-## -->
