31b4e489471f58aa18709498e63c48a57896ca25
邓渊 <330715487@qq.com>
2016-09-14 14:30:44 +0800

    1，更新了友情链接 2，新增了随机图片

--- template-parts/content-links.php
+++ template-parts/content-links.php
@@ -9,8 +9,8 @@
 				<ul class="cl">
 					<?php wp_list_bookmarks('title_li=&categorize=0&orderby=rand&limit=50'); ?>
 					<li><a rel="nofollow" href="http://www.wordpressleaf.com/"  target="_blank">WordPressLeaf</a> </li>
-					<li><a rel="nofollow" href="http://www.estory365.com/"  target="_blank">衣故事</a> </li>
-					<li><a rel="nofollow" href="http://mm2baby.taobao.com/"  target="_blank">亲亲 妈咪宝贝</a> </li>
+					<li><a rel="nofollow" href="http://www.py40.com/"  target="_blank">python在线学习</a> </li>
+					<li><a rel="nofollow" href="http://tools.py40.com/"  target="_blank">PY40在线工具</a> </li>
 				</ul>
 			</div>
 		</div>
