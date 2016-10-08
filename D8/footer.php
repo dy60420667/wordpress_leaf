7e28e7c760f9383eddd4de358ee280833bd42ff3
邓渊 <330715487@qq.com>
2016-10-08 08:13:58 +0800

    更新源码咯

--- D8/footer.php
+++ D8/footer.php
@@ -1,8 +1,13 @@
 </section>
 <footer class="footer">
+    
     <div class="footer-inner">
-        <div class="copyright pull-left">
-            版权所有，保留一切权利！ &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>　Theme <a href="http://www.daqianduan.com/" target="_blank"><?php echo get_current_theme(); ?></a>
+	<div style="padding-bottom:5px">快速导航：
+	<a href="http://www.py40.com/126.html" style="padding-right:15px">关于我们</a>|<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=330715487@qq.com" style="padding-left:15px;padding-right:15px">联系我们</a>|<a href="http://www.py40.com/130.html" style="padding-left:15px;padding-right:15px">申请友链</a>|<a href="http://work.py40.com" style="padding-left:15px;padding-right:15px">程序员驿站</a>|<a href="http://www.py40.com/sitemap.html" style="padding-left:15px;padding-right:15px">站点地图</a>
+       </div>
+	<div style="padding-bottom:5px">我们的服务：免费提供Python在线教程,Python资源,Python面试题,python算法等。</div>
+        <div class="copyright pull-left">网站信息：
+            版权所有，保留一切权利！ &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?>
         </div>
         <div class="trackcode pull-right">
             <?php if( dopt('d_track_b') ) echo dopt('d_track'); ?>
