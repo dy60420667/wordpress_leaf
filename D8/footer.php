ded52e5f569f547319272856aba91e3a7dae3af8
邓渊 <330715487@qq.com>
2016-10-09 17:06:31 +0800

    修改了

--- D8/footer.php
+++ D8/footer.php
@@ -1,6 +1,6 @@
 </section>
 <footer class="footer">
-    
+
     <div class="footer-inner">
 	<div style="padding-bottom:5px">快速导航：
 	<a href="http://www.py40.com/126.html" style="padding-right:15px">关于我们</a>|<a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=330715487@qq.com" style="padding-left:15px;padding-right:15px">联系我们</a>|<a href="http://www.py40.com/130.html" style="padding-left:15px;padding-right:15px">申请友链</a>|<a href="http://work.py40.com" style="padding-left:15px;padding-right:15px">程序员驿站</a>|<a href="http://www.py40.com/sitemap.html" style="padding-left:15px;padding-right:15px">站点地图</a>
@@ -9,14 +9,12 @@
         <div class="copyright pull-left">网站信息：
             版权所有，保留一切权利！ &copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?>
         </div>
-        <div class="trackcode pull-right">
-            <?php if( dopt('d_track_b') ) echo dopt('d_track'); ?>
-        </div>
+
     </div>
 </footer>
-<?php 
-wp_footer(); 
-if( dopt('d_footcode_b') ) echo dopt('d_footcode'); 
+<?php
+wp_footer();
+if( dopt('d_footcode_b') ) echo dopt('d_footcode');
 ?>
 </body>
 </html>
