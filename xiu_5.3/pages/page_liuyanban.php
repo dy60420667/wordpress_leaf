21d5bb48dcfc8ca07542dca2595b092c2be4e714
邓渊 <330715487@qq.com>
2017-04-20 16:07:52 +0800

    修改了几个主题

--- xiu_5.3/pages/page_liuyanban.php
+++ xiu_5.3/pages/page_liuyanban.php
@@ -8,10 +8,17 @@ get_header();
 
 <div class="content-wrap">
 	<div class="content">
+		<?php while (have_posts()) : the_post(); ?>
+		<h1 class="title"><strong><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></strong></h1>
+			<article class="article-content">
+				<?php the_content(); ?>
+			</article>
+		<?php endwhile;  ?>
 
 		<?php comments_template('/comments_liuyanban.php', true); ?>
 	</div>
 </div>
+
 
 <?php get_sidebar(); get_footer(); ?>
 
